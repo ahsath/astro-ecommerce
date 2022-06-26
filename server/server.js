9e660b70b6d526ea7f57fdfc2f0c7fb22a9d97f2
Ahsath <ahsath@gmail.com>
2022-06-26 10:25:44 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -3,20 +3,38 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/navbar', (req, res) => {
-  res.json(require('./data/navbar.json'))
+const categories = require('./data/categories.json')
+const collections = require('./data/collections.json')
+
+app.get('/categories', (req, res) => {
+  res.json(categories)
 })
 
-app.get('/collection', (req, res) => {
-  res.json(require('./data/collection.json'))
+app.get('/collections', (req, res) => {
+  res.json(collections)
 })
 
-app.get('/arrivals', (req, res) => {
-  res.json(require('./data/arrivals.json'))
+app.get('/collections/:collectionId', (req, res) => {
+  const collectionId = req.params.collectionId
+  const collection = collections.find(collection => collection.id === collectionId)
+  res.json(collection)
+})
+
+app.get('/products', (req, res) => {
+  res.json(require('./data/products.json'))
 })
 
 app.get('/seasons', (req, res) => {
   res.json(require('./data/seasons.json'))
+})
+
+app.get('/filters', (req, res) => {
+  const filters = [
+    { id: 'Todu', name: '', by: [{ name: 'Mostre Todu', pathname: '/Todu/1' }] },
+    { id: 'Categorias', name: 'Categorias', by: categories },
+    { id: 'Coleçāo', name: 'Coleçāo', by: collections }
+  ]
+  res.json(filters)
 })
 
 app.listen(port, () => {
