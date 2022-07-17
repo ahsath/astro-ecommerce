8514086d65fc92a4cfec63ac6a58cfaba3588543
Ahsath <ahsath@gmail.com>
2022-07-18 00:01:05 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ app.get('/products', (req, res) => {
 })
 
 app.get('/seasons', (req, res) => {
-  const seasons = ['winter', 'spring', 'autumn', 'summer']
   res.json(require('./data/seasons.json'))
 })
 
@@ -34,7 +33,7 @@ app.get('/filters', (req, res) => {
   const filters = [
     { id: 'Todu', name: '', by: [{ name: 'Mostre Todu', pathname: '/Todu/1' }] },
     { id: 'Categorias', name: 'Categorias', by: categories },
-    { id: 'Coleçāo', name: 'Coleçāo', by: tags }
+    { id: 'Coleçāo', name: 'Tag', by: tags }
   ]
   res.json(filters)
 })
